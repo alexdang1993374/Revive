1259e1dc9e4cf9abc98d099b1d25f5c98b672f3c
unknown <alexdang1993374@gmail.com>
2021-01-25 00:22:11 +0700

    fixed detail loading bug

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,21 +3,24 @@ import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 //Redux
-import {useDispatch} from 'react-redux';
-import {loadDetail} from '../actions/detailAction'
+import { useDispatch } from "react-redux";
+import { loadDetail } from "../actions/detailAction";
+import { Link } from "react-router-dom";
 
 const Game = ({ name, released, image, id }) => {
   //Load Details
   const dispatch = useDispatch();
   const loadDetailHandler = () => {
-    dispatch(loadDetail(id))
-  }
+    dispatch(loadDetail(id));
+  };
 
   return (
     <StyledGame onClick={loadDetailHandler}>
-      <h3>{name}</h3>
-      <p>{released}</p>
-      <img src={image} alt={name} />
+      <Link to={`/game/${id}`}>
+        <h3>{name}</h3>
+        <p>{released}</p>
+        <img src={image} alt={name} />
+      </Link>
     </StyledGame>
   );
 };
@@ -27,6 +30,7 @@ const StyledGame = styled(motion.div)`
   box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
   text-align: center;
   border-radius: 1rem;
+  cursor: pointer;
   img {
     width: 100%;
     height: 40vh;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import GameDetail from '../components/GameDetail'
+import GameDetail from "../components/GameDetail";
 //Redux
 import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
@@ -8,8 +8,12 @@ import Game from "../components/Game";
 //Styling and Animation
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
 
 const Home = () => {
+  //get the current location
+  const location = useLocation();
+  const pathId = location.pathname.split("/")[2];
   //Fetch Games
   const dispatch = useDispatch();
   useEffect(() => {
@@ -21,7 +25,7 @@ const Home = () => {
 
   return (
     <GameList>
-      <GameDetail />
+      {pathId && <GameDetail />}
       <h2>Upcoming Games</h2>
       <Games>
         {upcoming.map((game) => (
