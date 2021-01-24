44546e1a13bc9b97391edd35f344020110b11cc9
unknown <alexdang1993374@gmail.com>
2021-01-24 20:22:48 +0700

    set up state management

---
+++ src/reducers/gamesReducer.js
@@ -0,0 +1,27 @@
+
+const initState = {
+  popular: [],
+  newGames: [],
+  upcoming: [],
+  searched: [],
+};
+
+const gamesReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "FETCH_GAMES":
+      return { ...state };
+    default:
+      return { ...state };
+  }
+};
+
+//Action creator
+const fetchGames = (userData) => {
+  return {
+    type: "FETCH_GAMES",
+  };
+};
+
+
+
+export default gamesReducer;
